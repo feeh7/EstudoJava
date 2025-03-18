0961a779712864f13179b699fff2e6733df0f1a1
feeh7 <rafael.maria01@etec.sp.gov.br>
2025-03-18 12:22:13 -0300

    uptade ex 3

---
+++ IfElseEx/CondicaoTeste/src/CondicaoTesteTres.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CondicaoTesteTres {
+    public static void main(String[] args) {
+
+        Scanner keyandmouse = new Scanner(System.in);
+
+        System.out.println("Enter two numbers and in the end I will say which one is bigger or if they are the same!");
+
+        System.out.println("Enter the first number: ");
+        int number = keyandmouse.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int numberTwo = keyandmouse.nextInt();
+
+        if (number > numberTwo) {
+            System.out.println("The first number entered is higher than the second");
+        }
+        else {
+            System.out.println("The second number entered is higher than the first");
+        }
+        {
+            System.out.println("The numbers are equal");
+        }
+
+    }
+}
