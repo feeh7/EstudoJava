e23d7f6fcd7630e625716e0d846ccbb9d375fcf6
feeh7 <rafael.maria01@etec.sp.gov.br>
2025-03-18 12:01:11 -0300

    uptade ex

--- IfElseEx/CondicaoTeste/src/CondicaoTeste.java
+++ IfElseEx/CondicaoTeste/src/CondicaoTeste.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class CondicaoTeste {
     public static void main(String[] args) {
-        System.out.println();
+
+        Scanner keyandmouse = new Scanner(System.in);
+
+        System.out.println("Enter a number and say if it is positive or negative: ");
+        int number = keyandmouse.nextInt();
+
+        if (number >= 1) {
+            System.out.println("This number is positive and greater than zero");
+        }
+        else if (number < 0){
+            System.out.println("This number is negative");
+        }
+        else {
+            System.out.println("This number is zero");
+        }
     }
 }
