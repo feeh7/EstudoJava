a00b3fa39ade916e1dddf4e1fd10b33d967fb44f
feeh7 <rafael.maria01@etec.sp.gov.br>
2025-03-18 12:10:25 -0300

    uptade ex 2

--- IfElseEx/CondicaoTeste/src/CondicaoTesteDois.java
+++ IfElseEx/CondicaoTeste/src/CondicaoTesteDois.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class CondicaoTesteDois {
+    public static void main(String[] args) {
+
+        Scanner keyandmouse = new Scanner(System.in);
+
+        System.out.println("Type a number and say if it is odd or even: ");
+        int number = keyandmouse.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("This number is odd");
+        }
+        else {
+            System.out.println("This number is even");
+        }
+    }
 }
