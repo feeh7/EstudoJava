22c00f87dcee1c8b6ca7b8829c6db487655cbd55
feeh7 <rafael.maria01@etec.sp.gov.br>
2025-03-19 19:49:53 -0300

    Finalização ex 6

--- IfElseEx/CondicaoTeste/src/CondicaoTesteSeis.java
+++ IfElseEx/CondicaoTeste/src/CondicaoTesteSeis.java
@@ -5,8 +5,18 @@ public class CondicaoTesteSeis {
 
         Scanner keyandmouse = new Scanner(System.in);
 
-        System.out.println("Digite o salário do estágiario da sua empresa!");
-        System.out.println("");
+        System.out.println("Enter the salary of your company's stage!");
+        System.out.println("What is the salary of the Stagium?: ");
+        double value = keyandmouse.nextInt();
+
+        if (value < 2000) {
+            double endValue = (value * 1.10);
+            System.out.println("Promoted for hard work, now his salary is R$"+endValue);
+        }
+        else {
+            double endValue = (value * 1.05);
+            System.out.println("The other intern worked harder, but here, take a significant raise. Your salary is now R$"+endValue);
+        }
 
     }
 }
