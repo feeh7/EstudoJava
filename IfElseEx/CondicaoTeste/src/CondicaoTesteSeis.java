82377e79d4d9a3c6895226778a281ad1939c1308
feeh7 <rafael.maria01@etec.sp.gov.br>
2025-03-19 08:47:39 -0300

    ex 6 inicio

---
+++ IfElseEx/CondicaoTeste/src/CondicaoTesteSeis.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CondicaoTesteSeis {
+    public static void main(String[] args) {
+
+        Scanner keyandmouse = new Scanner(System.in);
+
+        System.out.println("Digite o salário do estágiario da sua empresa!");
+        System.out.println("");
+
+    }
+}
